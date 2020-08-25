d7378cea1057795e7a5bb9413a203e03e9325c14
tabaesso <tatabaesso@gmail.com>
2020-08-25 01:02:16 -0300

    eslint, prettier e editor config

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-   return response.json({ message: 'Hello World' });
-})
+app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
 app.listen(3333, () => {
-   console.log('Back tá on ✨');
+  console.log('Back tá on ✨');
 });
