f8c2e537ff8a85803462d8a7ac4ee8d1a3f8e573
tabaesso <tatabaesso@gmail.com>
2020-08-25 01:40:20 -0300

    fazendo o debug de um código no vscode

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,15 @@
 import { Router } from 'express';
 
 const routes = Router();
+
+routes.post('/', (request, response) => {
+  const { name, email } = request.body;
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+});
+
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
-
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Back tá on ✨');
