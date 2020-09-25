827f0923660b8879695cfc2b68ebcdc3f23f2e3b
tabaesso <tatabaesso@gmail.com>
2020-09-25 01:30:14 -0300

    criando teste de createAppointment

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,8 +1,17 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {
-    expect(1 + 2).toBe(3);
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(fakeAppointmentsRepository);
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '12315646'
+    });
+
+    expect(appointment).toHaveProperty('id');
   });
 
   it('should not be able to create two appointments on the same time', () => {
