76965f4b3b3ae4c47ee4c8d5820a7653af2a5c74
tabaesso <tatabaesso@gmail.com>
2021-07-11 17:23:44 -0300

    Enviando emails pelo amazon ses

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'tabaesso@tabaesso.com',
+      name: 'Tabs do GoStack',
+    },
+  },
+} as IMailConfig;
